947e94c482ec52c5b6e1dfaf2a4755d131f68156
melself <maskot2002@yandex.ru>
2022-11-01 20:20:19 +0300

    Fix

---
+++ mylibrary/build/generated/source/buildConfig/debug/com/melself/mylibrary/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.melself.mylibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.melself.mylibrary";
+  public static final String BUILD_TYPE = "debug";
+}
